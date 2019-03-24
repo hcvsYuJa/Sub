0c5f040e87e4830ce5f75bae1c52c1cb73239a18
hcvsYuJa <48265002+hcvsYuJa@users.noreply.github.com>
2019-03-24 13:03:24 +0800

    Update Bubble_Sort.cpp

--- Sort/Bubble_Sort.cpp
+++ Sort/Bubble_Sort.cpp
@@ -6,7 +6,7 @@ void sub_Bubble_Sort(int*array,int size)/*(陣列初始位址,陣列大小)*/
 	bool A=false;
 	for(i=0;i<size;i++)
 	{
-		if(i==(size-1)&&A==true)/*如果i到達(size-1)也就是該陣列最後一個值，並且A=true代表有改到陣列的內容，就將A=true,i=0，重新再跑一次*/
+		if(i==(size-1)&&A==true)/*如果i等於(size-1)也就是該陣列最後一個值，並且A=true代表有改到陣列的內容，就將A=true,i=0，重新再掃一次*/
 		{
 			A=false;
 			i=0;
@@ -14,7 +14,7 @@ void sub_Bubble_Sort(int*array,int size)/*(陣列初始位址,陣列大小)*/
 		if(array[i]>array[i+1]&&i<(size-1))/*只跑到(size-2)是因為(i+1)不只是跑到(size-2)的話(i+1)會超過陣列的大小所承受之範圍*/
 		{
 				/*以下三行負責將索引(i)和索引(i+1)的資料對調*/
-        /*1*/Ped=array[i];
+				/*1*/Ped=array[i];
 		 		/*2*/array[i]=array[i+1];
 		 		/*3*/array[i+1]=Ped;
 		 		A=true;/*A=true代表這次迴圈有改到陣列內容*/
