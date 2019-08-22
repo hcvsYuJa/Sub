38aa4c4a716889992a0cbcd4ca5800bf037b728b
hcvsYuJa <48265002+hcvsYuJa@users.noreply.github.com>
2019-08-22 17:38:51 +0800

    Create Quick_Sort.cpp

---
+++ Sort/Quick_Sort.cpp
@@ -0,0 +1,34 @@
+void change(int*a,int*b)
+{
+    int Val;
+    Val=*a;
+    *a=*b;
+    *b=Val;
+}
+void Quick_Sort(int*arr,int index_Key,int index_End)
+{
+    int Key=arr[index_Key];
+    if(index_Key<index_End)
+    {
+        int i=index_Key,j=index_End+1,sw=0;
+
+            do
+            {
+                do
+                {
+                    i=i+1;
+                }while(arr[i]<Key);
+                do
+                {
+                    j=j-1;
+                }while(arr[j]>Key);
+                    if(i<j)
+                        change(&arr[i],&arr[j]);
+                    //i=i+1;
+                    //j=j-1;
+            }while(i<j);
+            change(&arr[index_Key],&arr[j]);//兩值交換
+            Quick_Sort(arr,index_Key,j-1);
+            Quick_Sort(arr,j+1,index_End);
+    }
+}
